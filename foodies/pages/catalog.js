8d33be34c32cb279e180beb42f8b5b2f043ce9c2
kellyquintesa <kellyquintesa11@gmail.com>
2022-06-11 22:55:37 +0700

    add images on catalog.js

--- foodies/pages/catalog.js
+++ foodies/pages/catalog.js
@@ -1,8 +1,15 @@
 import Image from "next/image";
 
 export const Profile = () => (
-  <div className="container mx-auto p-10">
-    <Image src="/images/bg1.png" alt="my picture" width={500} height={150} />
+  <div className=" pt-20 pb-20">
+    <div className="container mx-auto flex flex-wrap justify-center ">
+      <Image src="/images/bg3.png" alt="my picture" width={800} height={250} />
+      <Image src="/images/bg2.png" alt="my picture" width={800} height={250} />
+      <Image src="/images/bg4.png" alt="my picture" width={800} height={250} />
+      <Image src="/images/bg5.png" alt="my picture" width={800} height={250} />
+      <Image src="/images/bg1.png" alt="my picture" width={800} height={250} />
+      <Image src="/images/bg6.png" alt="my picture" width={800} height={250} />
+    </div>
   </div>
 );
 
